4ac762863e2cbe82930d53a0ab8b0c16970091c8
vera73 <134825197+vera73@users.noreply.github.com>
2023-08-25 16:30:05 +0700

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <h2 className='about_title'>About me</h2>
+    <div className='about_descr'> My name is Vera. I'm 15 years old.</div>
+    </>
+  )
+}
